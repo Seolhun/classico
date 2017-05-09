9b85a448c4ccf66dcbfc8ef44ef949da369c1152
shun10114@gmail.com <shun10114@gmail.com>
2017-05-09 22:08:25 +0900

    commit python admin

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from backend.news import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('news.urls')),
+    url(r'^$', views.post_list, name='post_list'),
 ]
